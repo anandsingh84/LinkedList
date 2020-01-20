2d1de6354323b91b0fce62bc7c0d9a6871e0afe6
Anand  Singh <anandsingh84@gmail.com>
2020-01-20 02:57:31 +0000

    Append and Prepend method

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
 // Linked List Data strucute
-console.log("Creating LinkedList data strucute in JavaScript");
+
+class LinkedList{
+  constructor(value){
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+   // console.log(this);
+  }
+
+  append(value){
+    const newNode = {
+      value: value,
+      next: null
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value){
+    const newNode = {
+      value : value,
+      next: null
+    };
+
+    newNode.next = this.head;
+    this.head = newNode;        
+    this.length++;
+    return this;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(15);
+myLinkedList.append(5);
+myLinkedList.append(25);
+myLinkedList.prepend(20);
+myLinkedList.prepend(5);
